d029ff631dfe47e3f2e17a47dec41de5d488b01b
johann <nikola.tesla.130420@gmail.com>
2023-12-21 13:15:28 -0600

    falta terminar kata de tiempo

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,23 @@
 console.log('para hacer katas')
 
 function calculateTime (deliveries: string[]): string {
-  return '00:00:00'
+  let horas = 0
+  let minutos = 0
+  let segundos = 0
+  for (const delivery of deliveries) {
+    const [h, m, s] = delivery.split(':')
+    horas += (+h)
+    minutos += (+m)
+    segundos += (+s)
+  }
+  const segundosR = segundos % 60
+  const minutosR = (minutos + (Math.floor(segundos / 60))) % 60
+  const horasR = horas + Math.floor(minutos / 60)
+  if (horasR >= 7) {
+    return `0${horasR - 7}:${minutosR}:${segundosR}`
+  } else {
+    return `-${7 - horasR - 1}:${60 - minutosR}:${segundosR > 0 ? 60 - segundosR : 0}`
+  }
 }
 
 console.log(calculateTime(['00:10:00', '01:00:00', '03:30:00']))
